04603a14bd9c71c27a0d5e601a528d3ee852c110
SergeyShynkarenko <serg.shunkarenko@gmail.com>
2023-02-21 08:08:39 +0300

    add test page

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,24 @@
-//script
+document.addEventListener("DOMContentLoaded", () => {
+  const tabHeaders = document.querySelectorAll("[data-tab]");
+  const contentBoxes = document.querySelectorAll("[data-tab-content]");
+
+  tabHeaders.forEach(function (item) {
+    item.addEventListener("click", function () {
+      contentBoxes.forEach(function (item) {
+        item.classList.add("hidden");
+      });
+
+      const contentBox = document.querySelector("#" + this.dataset.tab);
+      contentBox.classList.remove("hidden");
+    });
+  });
+
+  for (let i = 0; i < tabHeaders.length; i++) {
+    tabHeaders[i].addEventListener("click", () => {
+      for (let jj = 0; jj < tabHeaders.length; jj++) {
+        tabHeaders[jj].classList.remove("item--active");
+      }
+      tabHeaders[i].classList.add("item--active");
+    });
+  }
+});
